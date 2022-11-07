71d253c849ca609c1442e96723d5db69bd12097f
chen <15305067103@163.com>
2022-11-07 09:30:57 +0800

    Initial commit

---
+++ src/main/java/com/example/newgameshop/NewGameShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.newgameshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGameShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewGameShopApplication.class, args);
+	}
+
+}
